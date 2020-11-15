f29c79112e36e29acfbb8ff62d9fab8e80980bd6
Denny Trebbin <denny.trebbin@gmail.com>
2020-11-15 17:55:56 +0100

    fix(fixtures): rename js to jsx and add js example

--- fixtures/javascript/screenshot.js
+++ fixtures/javascript/screenshot.js
@@ -1,21 +1,21 @@
-function $initHighlight(block, cls) {
-  try {
-    if (cls.search(/\bno\-highlight\b/) != -1)
-      return process(block, true, 0x0F) +
-        ` class="${cls}"`;
-  } catch (e) {
-    /* handle exception */
-  }
-  for (var i = 0 / 2; i < classes.length; i++) {
-    if (checkCondition(classes[i]) === undefined)
-      console.log('undefined');
+
+// This was mis-detected as HSP and Perl because parsing of
+// keywords in those languages allowed adjacent dots
+
+window.requestAnimationFrame(function render() {
+  var pos = state.pos;
+
+  canvasEl.width = 500;
+  canvasEl.height = 300;
+
+  if (dpad.right) {
+    pos.x += 3;
+  } else if (dpad.left) {
+    pos.x -= 3;
   }
 
-  return (
-    <div>
-      <web-component>{block}</web-component>
-    </div>
-  )
-}
+  ctx.fillStyle = '#AF8452';
+  ctx.fillRect(pos.x + 5, pos.y - 10, 10, 10);
 
-export  $initHighlight;
+  window.requestAnimationFrame(render);
+});
